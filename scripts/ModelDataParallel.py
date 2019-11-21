Marmousi gradients look correct

scripts/ModelDataParallel.py
@@ -9,7 +9,7 @@ and little endian (xdr_float vs native_float) the code
 for now requires big endian.
 
 @author: Joseph Jennings
-@version: 2019.11.13
+@version: 2019.11.20
 """
 from __future__ import print_function
 import sys, os, argparse, configparser
@@ -159,12 +159,16 @@ for isx in range(nsx):
   allsrcx[isx,0] = int(srcs[isx])
   allsrcz[isx,0] = int(srczp)
 
-#TODO: put back in the acquisition plotting
-#      need to think about the best way to do it
-#      Probably best way is if to check if number of receivers and sources
-#      don't change for each shot, then plot
+if(plotacq):
+  plt.figure(1)
+  # Plot velocity model
+  vmin = np.min(velp); vmax = np.max(velp)
+  plt.imshow(velp,extent=[0,nxp,nzp,0],vmin=vmin,vmax=vmax,cmap='jet')
+  # Get all source positions
+  plt.scatter(allrecx[0,:],allrecz[0,:])
+  plt.scatter(allsrcx[:,0],allsrcz[:,0])
 
-# Crete input wavelet array
+# Create input wavelet array
 allsrcs = np.zeros([nsx,1,ntu],dtype='float32')
 for isx in range(nsx):
   allsrcs[isx,0,:] = src[:]
@@ -181,7 +185,7 @@ sca.fwdprop_multishot(allsrcs,allsrcx,allsrcz,nsrc,allrecx,allrecz,nrec,nsx,velp
 
 ## Write out all shots
 datout = np.transpose(allshot,(1,2,0))
-daxes = seppy.axes([ntd,nrx,nsx],[0.0,orx*dx,osx*dx],[dt,drx*dx,dsx*dx])
+daxes = seppy.axes([ntd,nrx,nsx],[0.0,orx,osx],[dt,drx,dsx])
 sep.write_file("out",daxes,datout)
 
 ## Write auxiliary information to header

scripts/grad-comp.py
@@ -113,20 +113,20 @@ gradfd = np.zeros(vtrup.shape,dtype='float32')
 datptb = np.zeros(ntd,dtype='float32')
 
 # Calculate objective function perturbation for each point in the model
-for iz in range(5,nzp):
-  for ix in range(5,nxp):
-    vptbp[:] = vmodp[:]
-    vptbp[iz,ix] += dvel
-    sca.fwdprop_oneshot(fsrc,srcx,srcz,1,recx,recz,1,vptbp,datptb)
-    misptb = objf(datptb,dattru)
-    gradfd[iz,ix] = (misptb - mis0)/dvel
-
+#for iz in range(5,nzp):
+#  for ix in range(5,nxp):
+#    vptbp[:] = vmodp[:]
+#    vptbp[iz,ix] += dvel
+#    sca.fwdprop_oneshot(fsrc,srcx,srcz,1,recx,recz,1,vptbp,datptb)
+#    misptb = objf(datptb,dattru)
+#    gradfd[iz,ix] = (misptb - mis0)/dvel
+#
 sep = seppy.sep([])
-gaxes = seppy.axes([nzp,nxp],[0.0,0.0],[dz,dx])
-sep.write_file(None,gaxes,gradfd,'pygradfd1.H')
+#gaxes = seppy.axes([nzp,nxp],[0.0,0.0],[dz,dx])
+#sep.write_file(None,gaxes,gradfd,'pygradfd1.H')
 
-#gaxes,gradfd = sep.read_file(None,ifname='pygradfd.H')
-#gradfd = gradfd.reshape(gaxes.n,order='F')
+gaxes,gradfd = sep.read_file(None,ifname='pygradfd.H')
+gradfd = gradfd.reshape(gaxes.n,order='F')
 
 cmin = np.min(gradfd); cmax = np.max(gradfd)
 
9bf8043444e373293ef7e341f064271d66cfeefe
Joseph Jennings <joseph.s.jennings@gmail.com>
2019-11-22 15:42:40 -0800

    A gradient taper operator

---
+++ py/scaas/gradtaper.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def build_taper(nx,nz,z1,z2):
+  tap1d = np.zeros(nz,dtype='float32')
+  # Create taper in depth
+  for iz in range(nz):
+    if(iz < z1):
+      tap1d[iz] = 0.0
+    elif(iz >= z1 and iz <= z2):
+      tap1d[iz] = np.cos(np.pi/2*(z2-iz)/(z2-z1))**2
+    elif(iz >= z2):
+      tap1d[iz] = 1.0
+  return tap1d,np.tile(np.array([tap1d]).T,(1,nx))
+

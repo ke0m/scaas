4ae9d2591b43f0f716ce372c05117c49e25a2d87
ke0m <joseph.s.jennings@gmail.com>
2020-03-01 16:56:44 -0800

    Now handles thread numbers greater than 10

--- scaas/src/scaas2d.cpp
+++ scaas/src/scaas2d.cpp
@@ -1153,10 +1153,15 @@ void scaas2d::printprogress_omp(std::string prefix, int icur, int tot, int threa
   double percentage = (double)icur/tot;
   int lpad = (int) (percentage * PBWIDTH);
   int rpad = PBWIDTH - lpad-1;
-  printf ("\r(thd: %d ) %s [%.*s>%*s] %d/%d", thread, prefix.c_str(), lpad, PBSTR, rpad, "", icur, tot);
+  std::string tid;
+  if(thread < 10) {
+    tid = std::string( 1, '0').append(std::to_string(thread));
+
+  }
+  printf ("\r(thd: %s) %s [%.*s>%*s] %d/%d", tid.c_str(), prefix.c_str(), lpad, PBSTR, rpad, "", icur, tot);
   fflush (stdout);
   if(icur == tot-1) {
-    printf ("\r(thd: %d) %s [%.*s%*s] %d/%d", thread, prefix.c_str(), PBWIDTH, PBSTR, 0, "", tot, tot);
+    printf ("\r(thd: %s) %s [%.*s%*s] %d/%d", tid.c_str(), prefix.c_str(), PBWIDTH, PBSTR, 0, "", tot, tot);
     printf(" ");
   }
 }

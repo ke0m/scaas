541b883c0ac449cf7544ea31d0966203431fb0ef
ke0m <joseph.s.jennings@gmail.com>
2020-03-22 22:58:07 -0700

    Removed debug statements

--- bench/scripts/testoff2ang.py
+++ bench/scripts/testoff2ang.py
@@ -21,26 +21,30 @@ datt = np.ascontiguousarray(dat.T).astype('float32')
 # Window just one image point
 iimg = datt[512,:,:]
 
-nta = 151; ota = -3; dta = 0.04
+#nta = 151; ota = -3; dta = 0.04
 #nta = 301; ota = -3; dta = 0.02
-#nta = 601; ota = -3; dta = 0.01
+nta = 601; ota = -3; dta = 0.01
 nro = 1
-na = 101; oa = -70; da = 1.4
+#na = 101; oa = -70; da = 1.4
+na = 281; oa = -70; da = 0.5
 
 print(ota,ota+(nta-1)*dta)
 print(oa,oa+(na-1)*da)
 
-#iang = np.zeros([na,nt],dtype='float32')
-angs = np.zeros([nx,na,nt],dtype='float32')
+iang = np.zeros([na,nt],dtype='float32')
+#angs = np.zeros([nx,na,nt],dtype='float32')
 
 #plt.figure()
 #plt.imshow(iimg,aspect='auto'); plt.show()
-#convert2ang(1,1,nh,oh,dh,nta,ota,dta,na,oa,da,nt,ot,dt,4,iimg,iang)
+convert2ang(1,1,nh,oh,dh,nta,ota,dta,na,oa,da,nt,ot,dt,4,iimg,iang)
 
-#plt.imshow(iang.T,cmap='gray')
-#plt.show()
+plt.imshow(iang.T,cmap='gray')
+plt.show()
 
-convert2ang(1,nx,nh,oh,dh,nta,ota,dta,na,oa,da,nt,ot,dt,4,datt,angs)
+#convert2ang(1,nx,nh,oh,dh,nta,ota,dta,na,oa,da,nt,ot,dt,4,datt,angs)
 
-viewimgframeskey(angs,pclip=0.5)
+#viewimgframeskey(angs,pclip=0.5)
+
+#oaxes = seppy.axes([nt,na,nx],[ot,oa,ox],[dt,da,dx])
+#sep.write_file(None,oaxes,angs.T,ofname='myangs.H')
 
--- scaas/src/adcig.cpp
+++ scaas/src/adcig.cpp
@@ -3,16 +3,11 @@
 #include "slantstk.h"
 #include "tan2ang.h"
 #include <string>
-//#include "matplotlibcpp.h"
-
-//namespace plt = matplotlibcpp;
 
 void convert2ang(int nro, int nx, int nh, float oh, float dh,
     int nta, float ota, float dta, int na, float oa, float da, int nz, float oz, float dz,
     int ext, float *off, float *ang) {
 
-   //std::map<std::string, std::string> keywords;
-   //keywords["aspect"] = "auto"; keywords["cmap"] = "gray";
   /* Build the slant stack operator */
   slantstk ssk = slantstk(true, nh, oh, dh, nta, ota, dta, nz, oz, dz, 0.0, 1.0);
 
@@ -24,13 +19,11 @@ void convert2ang(int nro, int nx, int nh, float oh, float dh,
   for(int iro = 0; iro < nro; ++iro) {
     /* Loop over image points (CDPs) */
     for(int ix = 0; ix < nx; ++ix) {
-      fprintf(stderr,"ix=%d\n",ix);
+      //TODO: put in a progressbar here
       /* Grab a single offset gather */
       float *gat = off + iro*nx*nh*nz + ix*nh*nz;
-      //plt::imshow((const float*)gat, nh, nz, 1,keywords); plt::show();
       /* Create the tangent gather */
       ssk.adjoint(false, nta*nz, nh*nz, tan, gat);
-      //plt::imshow((const float*)tan, nta, nz, 1,keywords); plt::show();
       /* Convert to angle */
       tan2ang(nz, nta, ota, dta, na, oa, da, ext, tan, ang + iro*nx*na*nz + ix*na*nz);
     }

2d8323ea632428e41d3f77b1c40edd990c8f9086
ke0m <joseph.s.jennings@gmail.com>
2020-03-22 22:24:30 -0700

    Forgot to add offset to angle conversio

---
+++ scaas/src/adcig.cpp
@@ -0,0 +1,35 @@
+#include <omp.h>
+#include <stdio.h>
+#include "adcig.h"
+#include "slantstk.h"
+#include "tan2ang.h"
+
+void convert2ang(int nro, int nx, int nh, float oh, float dh,
+    int nta, float ota, float dta, int na, float oa, float da, int nz, float oz, float dz,
+    int ext, float *off, float *ang, int nthrd, bool verb) {
+
+  /* Build the slant stack operator */
+  slantstk ssk = slantstk(true, nh, oh, dh, nta, ota, dta, nz, oz, dz, 0.0, 1.0);
+
+  /* Array for holding tangent gather*/
+  float *tan = new float[nta*nz]();
+
+  omp_set_num_threads(nthrd);
+#pragma omp parallel for default(shared)
+  /* Loop over residually migrated images */
+  for(int iro = 0; iro < nro; ++iro) {
+    /* Loop over image points (CDPs) */
+    for(int ix = 0; ix < nx; ++ix) {
+      //TODO: put progress bar here
+      /* Grab a single offset gather */
+      float *gat = off + iro*nx*nh*nz + ix*nh*nz;
+      /* Create the tangent gather */
+      ssk.adjoint(false, nta*nz, nh*nz, tan, gat);
+      /* Convert to angle */
+      tan2ang(nz, nta, ota, dta, na, oa, da, ext, tan, ang + iro*nx*na*nz + ix*na*nz);
+    }
+  }
+
+  /* Free memory */
+  delete[] tan;
+}

fb0c057e604c6923280ab977a91ec039ad8f8c8a
ke0m <joseph.s.jennings@gmail.com>
2020-03-24 22:42:51 -0700

    Merging first notebook commit

--- scaas/wavelet.py
+++ scaas/wavelet.py
@@ -1,5 +1,9 @@
+"""
+Source time functions for wave propagation
+@author: Joseph Jennings
+@version: 2020.03.24
+"""
 import numpy as np
-from utils.signal import butter_bandpass_filter
 
 def ricker(nt,dt,f,amp,dly):
   """ 
@@ -7,10 +11,10 @@ def ricker(nt,dt,f,amp,dly):
   returns a ricker wavelet
 
   Parameters
-    nt  - length of wavelet
-    dt  - sampling interval
-    f   - center frequency of wavelet
-    amp - peak amplitude of wavelet
+    nt  - length of output wavelet
+    dt  - sampling rate of wavelet
+    f   - dominant frequency of ricker wavelet
+    amp - maximum amplitude of wavelet
     dly - time delay of wavelet
   """
   src = np.zeros(nt,dtype='float32')
@@ -21,33 +25,43 @@ def ricker(nt,dt,f,amp,dly):
 
   return src
 
-def butterworth(nt,dt,locut,hicut,amp,dly):
-  """ 
-  Creates a zero-phase butterworth wavelet 
+def bandpass(nt,dt,fs,amp,dly):
+  """
+  Generates a bandpass ricker in the frequency domain
+  Basically a copy of Ali Almomin's Wavelet.f90
 
   Parameters
-    nt    - length of wavelet
-    dt    - sampling interval
-    hicut - high-cut of butterworth filter
-    locut - low-cut of butterworth filter
-    amp   - peak amplitude of wavelet
-    dly   - time delay of wavelet
+    nt  - length of output wavelet
+    dt  - sampling rate of wavelet
+    fs  - list of four frequencies that define band
+    amp - maximum amplitude of wavelet
+    dly - time delay of wavelet
   """
-  pass
+  df = 1/(nt*dt)
+  shift = dly/dt
+  wavw = np.zeros(nt,dtype='float32')
 
-def zerophase(nt,dt,f1,f2,f3,f4,amp,dly):
-  """ 
-  Creates a zero-phase wavelet based on tapered window function 
+  # Build spectrum in frequency domain
+  for i in range(int(nt/2)):
+    f = (i-1)*df
+    if(f < fs[0]):
+      wavw[i] = 0
+    elif(f >= fs[0] and f < fs[1]):
+      wavw[i] = np.cos(np.pi/2*(fs[1]-f)/(fs[1]-fs[0]))**2
+    elif(f >= fs[1] and f < fs[2]):
+      wavw[i] = 1.0
+    elif(f >= fs[2] and f < fs[3]):
+      wavw[i] = np.cos(np.pi/2*(f-fs[2])/(fs[3]-fs[2]))**2
+    else:
+      wavw[i] = 0
 
-  Parameters
-    nt  - length of wavelet
-    dt  - sampling interval
-    f1  - lowest frequency to be used in window
-    f2  - low corner frequency for defining the taper
-    f3  - high corner frequency for defining taper
-    f4  - higest frequency in window
-    amp - peak amplitude of wavelet
-    dly - time delay of the wavelet
-  """
-  pass
+  # Inverse FFT
+  wavtsh = np.real(np.fft.ifft(wavw))
+  wavt = amp*np.fft.fftshift(wavtsh)/np.max(wavtsh)
+
+  # Apply delay
+  t0 = int((nt/2))*dt
+  wavtd = np.roll(wavt,int((t0+dly)/dt))
+
+  return wavtd
 

b7f52a62adbad4ad9085a85a397511d65b5dd219
ke0m <joseph.s.jennings@gmail.com>
2020-04-26 12:34:25 -0700

    Extended smoothing now to 4D

--- scaas/trismooth.py
+++ scaas/trismooth.py
@@ -1,12 +1,12 @@
 """
-Three-dimensional triangular smoother
+Four-dimensional triangular smoother
 @author: Joseph Jennings
 @version: 2020.03.18
 """
 import numpy as np
 from scaas.trismoothkern import smooth2
 
-def smooth(data,rect1=1,rect2=1,rect3=1):
+def smooth(data,rect1=1,rect2=1,rect3=1,rect4=1):
   """
   Applies a triangular smoother along specified
   dimensions
@@ -15,7 +15,8 @@ def smooth(data,rect1=1,rect2=1,rect3=1):
     data  - The input data
     rect1 - Number of points to smooth along fast axis [-1, no smoothing]
     rect2 - Number of points to smooth along second axis [-1, no smoothing]
-    rect3 - Number of points to smooth along slow axis [-1, no smoothing]
+    rect3 - Number of points to smooth along third axis [-1, no smoothing]
+    rect4 - Number of points to smooth along slow axis [-1, no smoothing]
   """
   # First check that the input is of type float32
   if(data.dtype != np.zeros(1,dtype='float32').dtype):
@@ -23,10 +24,10 @@ def smooth(data,rect1=1,rect2=1,rect3=1):
 
   # Create the necessary inputs for the kernel function
   dim = len(data.shape)
-  ns    = np.ones(3,dtype='int32')
-  rects = np.ones(3,dtype='int32')
-  s     = np.zeros(3,dtype='int32')
-  rects[0] = rect1; rects[1] = rect2; rects[2] = rect3
+  ns    = np.ones(4,dtype='int32')
+  rects = np.ones(4,dtype='int32')
+  s     = np.zeros(4,dtype='int32')
+  rects[0] = rect1; rects[1] = rect2; rects[2] = rect3; rects[3] = rect4
 
   dim1 = -1
   for i in range(dim):

4f3ebbbe739424bf350f4bffd9b690be82f9ed05
ke0m <joseph.s.jennings@gmail.com>
2020-07-17 17:52:17 -0700

    Writes windowed data

--- bench/scripts2/sigsbee_mig2.py
+++ bench/scripts2/sigsbee_mig2.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 sep = seppy.sep()
 
 # Read in data
-daxes,dat = sep.read_file("sigsbee_shotflat.H")
+daxes,dat = sep.read_file("sigsbee_shotflatwind.H")
 dat = np.ascontiguousarray(dat.reshape(daxes.n,order='F').T).astype('float32')
 [nt,ntr] = daxes.n; [ot,_] = daxes.o; [dt,_] = daxes.d
 
 # Read in velocity model
-vaxes,vel = sep.read_file("sigsbee_vel.H")
+vaxes,vel = sep.read_file("sigsbee_velj4.H")
 vel = vel.reshape(vaxes.n,order='F')
 [nz,nx] = vaxes.n; [dz,dx] = vaxes.d; [oz,ox] = vaxes.o
 ny = 1; dy = 1.0
@@ -20,9 +20,9 @@ velin = np.zeros([nz,ny,nx],dtype='float32')
 velin[:,0,:] = vel
 
 # Read in coordinates
-saxes,srcx = sep.read_file("sigsbee_srcxflat.H")
-raxes,recx = sep.read_file("sigsbee_recxflat.H")
-_,nrec = sep.read_file("sigsbee_nrec.H")
+saxes,srcx = sep.read_file("sigsbee_srcxflatwind.H")
+raxes,recx = sep.read_file("sigsbee_recxflatwind.H")
+_,nrec = sep.read_file("sigsbee_nrecwind.H")
 nrec = nrec.astype('int')
 
 wei = geom.coordgeom(nx,dx,ny,dy,nz,dz,ox=ox,nrec=nrec,srcxs=srcx,recxs=recx)
@@ -32,5 +32,5 @@ waxes,wav = sep.read_file("sigwav.rsf",form='native')
 img = wei.image_data(dat,dt,wav=wav,ntx=16,minf=1,maxf=51,jf=3,vel=velin,nrmax=20,
                      nthrds=24,wverb=False)
 
-sep.write_file("mysigimg.H",img.T,ds=[dx,dy,dz],os=[ox,0.0,oz])
+sep.write_file("mysigimgwind.H",img.T,ds=[dx,dy,dz],os=[ox,0.0,oz])
 

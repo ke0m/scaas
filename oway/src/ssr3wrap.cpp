ced19cbb9fd5ccbceb619cdc3771b042addd218a
ke0m <joseph.s.jennings@gmail.com>
2020-07-25 23:43:57 -0700

    Beginnings of ssr3 wrappers

---
+++ oway/src/ssr3wrap.cpp
@@ -0,0 +1,20 @@
+#include "ssr3wrap.h"
+#include "ssr3.h"
+
+
+void ssr3_modshots(){
+
+}
+
+void ssr3_migshots() {
+
+}
+
+void ssr3_migoffshots(){
+
+}
+
+
+
+
+
---
+++ oway/src/ssr3wrap.h
@@ -0,0 +1,16 @@
+/**
+ * Wrapper functions for the ssr3 class
+ *
+ * @author: Joseph Jennings
+ * @version: 2020.07.25
+ */
+
+
+#ifndef SSR3WRAP_H_
+#define SSR3WRAP_H_
+
+void ssr3_modshots();
+void ssr3_migshots();
+void ssr3_migoffshots();
+
+#endif /* SSR3WRAP_H_ */

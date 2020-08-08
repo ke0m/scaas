bb3c431b0414dc5eda5d681097619316a9598ae5
ke0m <joseph.s.jennings@gmail.com>
2020-08-07 20:50:38 -0700

    K-space off2ang is working for one gather

--- scaas/src/ctrist.h
+++ scaas/src/ctrist.h
@@ -1,22 +1,35 @@
 /**
  * A complex tridiagonal solver.
- * Port of pctrist.f90 in SEPlib written by Paul Sava
+ * A port of pctrist.f90 in SEPlib written by Paul Sava
  * @author: Joseph Jennings
  * @version: 2020.08.05
  */
 #ifndef CTRIST_H_
 #define CTRIST_H_
 
+#include <complex.h>
+
 class ctrist {
 
   public:
-    ctrist(int nd, float od, float dd, int nm, float om, float dm, float eps);
+    ctrist(int n, float o, float d, float eps);
+    void define(float *coord);
+    void apply(std::complex<float> *model, std::complex<float> *data);
+    void solve(std::complex<float> *diag, std::complex<float> *offd, std::complex<float> *model);
+    ~ctrist(){
+      delete[] _idx; delete[] _flg;
+      delete[] _wgt; delete[] _diag; delete[] _offd;
+      delete[] _dd;  delete[] _oo;
+    }
 
   private:
+    int _nm;
+    float _om, _dm, _eps;
     int   *_idx;
     bool  *_flg;
-    float *_wgt;
-    float *_diag, *_offd;
+    std::complex<float> *_wgt;
+    std::complex<float> *_diag, *_offd;
+    std::complex<float> *_dd, *_oo;
 };
 
 

b18b331e27821481b574fea5def5836b5de2bd57
ke0m <joseph.s.jennings@gmail.com>
2020-08-21 16:57:43 -0700

    Merging image and model worker from master. Updating for SLURM cluster

---
+++ oway/imageworker.py
@@ -0,0 +1,39 @@
+"""
+Worker for imaging with one-way wave equation
+
+@author: Joseph Jennings
+@version: 2020.08.18
+"""
+import zmq
+from comm.sendrecv import notify_server, send_zipped_pickle, recv_zipped_pickle
+from oway.coordgeomchunk import coordgeomchunk
+
+# Connect to socket
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://maz-login01:5555")
+
+# Listen for work from server
+while True:
+  # Notify we are ready
+  notify_server(socket)
+  # Get work
+  chunk = recv_zipped_pickle(socket)
+  # If chunk is empty, keep listening
+  if(chunk == {}):
+    continue
+  # If I received something, do some work
+  ochunk = {}
+  # Create the modeling object
+  wei = coordgeomchunk(**chunk[0])
+  # Do the modeling
+  ochunk['result'] = wei.image_data(**chunk[1])
+  # Return other parameters if desired
+  ochunk['cid'] = chunk[2]
+  # Tell server this is the result
+  ochunk['msg'] = "result"
+  # Send back the result
+  send_zipped_pickle(socket,ochunk)
+  # Receive 'thank you'
+  socket.recv()
+
